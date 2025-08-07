e45e9c6180c2e31f2373e6834fdb5eaf442e7f62
Zippity <i.wa.anderson.86@gmail.com>
2025-08-06 21:23:46 -0700

    rewrite Grooming Station container registration method to follow TFC convention

--- src/main/java/cy/jdkdigital/tfcgroomer/Groomer.java
+++ src/main/java/cy/jdkdigital/tfcgroomer/Groomer.java
@@ -6,18 +6,18 @@ import cy.jdkdigital.tfcgroomer.common.block.GroomingStation;
 import cy.jdkdigital.tfcgroomer.common.block.entity.GroomingStationBlockEntity;
 import cy.jdkdigital.tfcgroomer.common.item.GroomingStationItem;
 import cy.jdkdigital.tfcgroomer.inventory.GroomingStationContainer;
+import net.dries007.tfc.common.blockentities.InventoryBlockEntity;
 import net.dries007.tfc.common.blocks.ExtendedProperties;
+import net.dries007.tfc.common.container.BlockEntityContainer;
 import net.dries007.tfc.util.Metal;
-import net.minecraft.core.BlockPos;
+import net.dries007.tfc.util.registry.RegistrationHelpers;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
-import net.minecraftforge.common.extensions.IForgeMenuType;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -30,6 +30,7 @@ import org.slf4j.Logger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Supplier;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Groomer.MODID)
@@ -59,14 +60,8 @@ public class Groomer
         });
     }
 
-    public static final RegistryObject<MenuType<GroomingStationContainer>> GROOMING_STATION_MENU = CONTAINER_TYPES.register("grooming_station", () -> {
-        return IForgeMenuType.create((windowId, playerInventory, buffer) -> {
-            Level level = playerInventory.player.level();
-            BlockPos pos = buffer.readBlockPos();
-            GroomingStationBlockEntity entity = level.getBlockEntity(pos, GROOMING_STATION_BLOCK_ENTITY.get()).orElseThrow();
-            return GroomingStationContainer.create(entity, playerInventory, windowId);
-        });
-    });
+    @SuppressWarnings("RedundantTypeArguments") // Following TFC registration convention
+    public static final RegistryObject<MenuType<GroomingStationContainer>> GROOMING_STATION_MENU = Groomer.<GroomingStationBlockEntity, GroomingStationContainer>registerBlock("grooming_station", GROOMING_STATION_BLOCK_ENTITY, GroomingStationContainer::create);
 
     public Groomer() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -88,4 +83,10 @@ public class Groomer
             });
         }
     }
+
+    private static <T extends InventoryBlockEntity<?>, C extends BlockEntityContainer<T>> RegistryObject<MenuType<C>> registerBlock(String name, Supplier<BlockEntityType<T>> type, BlockEntityContainer.Factory<T, C> factory)
+    {
+        return RegistrationHelpers.registerBlockEntityContainer(CONTAINER_TYPES, name, type, factory);
+    }
+
 }
